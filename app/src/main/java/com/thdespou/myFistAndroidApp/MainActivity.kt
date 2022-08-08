819473a9df0c5dd39f71c04742e74237fe2671fb
theodesp <328805+theodesp@users.noreply.github.com>
2022-08-08 15:01:21 +0100

    Add second screen
    Signed-off-by: theodesp <328805+theodesp@users.noreply.github.com>

--- app/src/main/java/com/thdespou/myFistAndroidApp/MainActivity.kt
+++ app/src/main/java/com/thdespou/myFistAndroidApp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.thdespou.myFistAndroidApp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+const val EXTRA_MESSAGE = "com.thdespou.myFistAndroidApp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
